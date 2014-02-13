2bf8597eda39ca634036a7ef63721275a91af0e3
Jeff Cohen <jeff@starterleague.com>
2014-02-13 10:47:36 -0600

    Ready for class

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,43 @@
+class ProductsController < ApplicationController
+
+  def index
+    @product_list = Item.all
+  end
+
+  def show
+    @product = Item.find_by(:id => params[:product_id])
+  end
+
+  def destroy
+    product = Item.find_by(:id => params[:product_id])
+    product.destroy
+  end
+
+  def new
+  end
+
+  def create
+    product = Item.new
+    product.name = params["name"]
+    product.price = params["price"]
+    product.description = params["description"]
+    product.image_url = params["image_url"]
+    product.save
+    redirect_to "/products"
+  end
+
+  def edit
+    @product = Item.find_by(:id => params[:product_id])
+  end
+
+  def update
+    product = Item.find_by(:id => params[:product_id])
+    product.name = params["name"]
+    product.price = params["price"]
+    product.description = params["description"]
+    product.image_url = params["image_url"]
+    product.save
+    redirect_to "/products"
+  end
+
+end
